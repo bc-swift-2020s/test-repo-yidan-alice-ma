9ed34935124beecbc3ad3c96e3396cf0ea55e3c1
Yidan Ma <madt@bc.edu>
2020-01-13 18:39:05 -0500

    weekone

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by mayidan on 1/13/20.
+//  Copyright © 2020 mayidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
